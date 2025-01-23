e8d86e80fed2a771fc5fb0542ace2500a88a8fbd
Seongjko <rhtjdwns2345@gmail.com>
2025-01-23 16:18:48 +0900

    checked successful Integration Firebase into the Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  // Flutter 바인딩 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+  // Firebase 초기화
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    debugPrint('Firebase 초기화 성공!');
+  } catch (e) {
+    debugPrint('Firebase 초기화 실패: $e');
+  }
   runApp(const MainApp());
 }
 
