2b65870c53be963a52a1f434d94350621ea287cc
Seongjko <rhtjdwns2345@gmail.com>
2025-01-24 15:27:33 +0900

    refactor: refactored signupscreen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -7,6 +7,7 @@ import 'package:firebase_storage/firebase_storage.dart' as fstorage;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weathercloset/test.dart';
 import '../../widgets/custom_text_field.dart';
 import '../../widgets/error_dialog.dart';
 import '../../widgets/loading_dialog.dart';
@@ -34,19 +35,21 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          const SizedBox(height: 10),
-          addProfilePhoto(context),
-          const SizedBox(height: 10),
-          signUpForm(),
-          const SizedBox(height: 10),
-          signUpButton(),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 10),
+            addProfilePhoto(context),
+            const SizedBox(height: 10),
+            signUpForm(),
+            const SizedBox(height: 10),
+            signUpButton(),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
       )
     );
   }
@@ -58,10 +61,11 @@ class _SignupScreenState extends State<SignupScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 12),
           ),
           onPressed: () {
+            debugPrint("[가입신청 버튼]을 누름");
             formValidation();
           },
           child: const Text(
-            "회원가입",
+            "가입신청",
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
@@ -139,7 +143,7 @@ class _SignupScreenState extends State<SignupScreen> {
 
   Future<void> formValidation() async {
     if (imageXFile == null) {
-      showDialog(
+      return showDialog(
           context: context,
           builder: (c) {
             return const ErrorDialog(
@@ -151,6 +155,7 @@ class _SignupScreenState extends State<SignupScreen> {
         if (confirmPasswordController.text.isNotEmpty &&
             emailController.text.isNotEmpty && 
             nameController.text.isNotEmpty) {
+          debugPrint("모든 입력 속성이 잘 입력되었음");
           showDialog(
               context: context,
               builder: (c) {
@@ -158,13 +163,15 @@ class _SignupScreenState extends State<SignupScreen> {
                   message: "계정 생성 중",
                 );
               });
+          debugPrint("계정생성 시작");
+
+          authenticateUserAndSignUp();
           String fileName = DateTime.now().millisecondsSinceEpoch.toString();
           fstorage.Reference reference = fstorage.FirebaseStorage.instance.ref().child("users").child(fileName);
           fstorage.UploadTask uploadTask = reference.putFile(File(imageXFile!.path));
           fstorage.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
           await taskSnapshot.ref.getDownloadURL().then((url) {
             userImageUrl = url;
-            authenticateUserAndSignUp();
           });
         } else {
           showDialog(
@@ -207,10 +214,13 @@ class _SignupScreenState extends State<SignupScreen> {
           });
     });
     if (currentUser != null) {
+      debugPrint("성공적으로 새 유저 생성 완료");
       saveDataToFireStore(currentUser!).then((value) {
+        //formValidation에서 생성한 dialog 제거
         Navigator.pop(context);
-        //kullanıcıyı anasayfaya yönlendirir.
-        Route newRoute = MaterialPageRoute(builder: (c) => const SignupScreen()); // title parametresi belirtilmedi
+        //signup이 완료되었으니 메인 화면으로 갈 준비
+        Route newRoute = MaterialPageRoute(builder: (c) => const TestWidget(text: "main",));
+        //스택 상 최상위 페이지를 signupscreen -> newRoute로 교체
         Navigator.pushReplacement(context, newRoute);
       });
     }
